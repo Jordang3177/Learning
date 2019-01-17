2110abd1d2624f85585740f911d327390437e7b1
Jordan Graves <Jordang3177@gmail.com>
2019-01-17 15:06:15 -0500

    Python Files

---
+++ py1.py
@@ -0,0 +1,15 @@
+word1 = "hello"
+word2 = " there"
+word3 = "another"
+word4 = " word"
+word5 = "last"
+word6 = " one"
+number1 = "5"
+number2 = "10"
+print('Result for first Calculation is: %d + %d = %d' % (10, 3, 10+3))
+print('Result for second Calculation is: %d * %d = %d' % (13, 34, 13*34))
+print('Result for third Calculation is: %d + %d + %d * %d = %d' % (11, 10, 100, 340, 11+10+100*340))
+print(word1+word2)
+print(word3+word4)
+print(word5+word6)
+print(int(number1) + int(number2))
---
+++ py2.py
@@ -0,0 +1,5 @@
+name = "Jordan Graves"
+if len(name) > 20:
+	print("The name that was input is over 20 characters")
+else:
+	print("The name that was input is under 20 characters")
