5ffcd2f5d8df57c69352d4ad298cdf0f93698473
Jordang3177 <graves5@indiana.edu>
2019-09-01 03:40:37 -0400

    August 31st Update

--- rn-starter/rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/rn-starter/src/screens/ComponentsScreen.js
@@ -1,6 +1,24 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-    return <Text></Text>
+    const name = 'Jordan'
+    
+    return (
+    <View>
+        <Text style={styles.textStyle}>Getting started with React Native!</Text>
+        <Text style={styles.textStyle2}>My name is {name}</Text>
+    </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 45
+    },
+    textStyle2: {
+        fontSize: 20
+    }
+});
+
+export default ComponentsScreen;
