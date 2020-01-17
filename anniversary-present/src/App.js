7a7d364bd988c929d4f85cd4e44865fcad1db49d
Jordan Graves <Jordang3177@gmail.com>
2020-01-17 23:18:16 +0800

    removed the css file for App.js as I can't see a need for one. If one is needed will add back in later

--- anniversary-present/src/App.js
+++ anniversary-present/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import Layout from './hoc/Layout/Layout';
 import Present from './containers/Present/Present';
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={classes.App}>
         <Layout>
           <Present />
         </Layout>
