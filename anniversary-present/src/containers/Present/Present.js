ff8983976fdd6405552bc477949d38ad1c1aee38
Jordan Graves <Jordang3177@gmail.com>
2020-01-17 23:12:53 +0800

    Added Present.js which will act as the main way for me to add content to the page and manipulate state

---
+++ anniversary-present/src/containers/Present/Present.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux/Aux';
+
+
+class Present extends Component {
+    render() {
+        return (
+            <Aux>
+
+            </Aux>
+        );
+    }
+}
+
+export default Present;
