60b4875b7085104ba135d436beeffd60cd8cdab4
Jordan Graves <Jordang3177@gmail.com>
2020-01-19 18:23:30 +0800

    Need to figure out how to add a text file as a variable in React. Will finish that up tomorrow

--- anniversary-present/src/containers/Timeline/Timeline.js
+++ anniversary-present/src/containers/Timeline/Timeline.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import YearElement from '../../Components/YearElement/YearElement';
 import BranchingLine from '../../Components/BranchingLine/BranchingLine';
+import Thing from '../../Assets/Text/Year1/Year1.txt'
+
 
 class Timeline extends Component {
     render() {
         var n = this.props.location.pathname.length
         var title = this.props.location.pathname.slice(1, 5) + " " + this.props.location.pathname.slice(5, n);
+        const Year1Text = Thing;
+        console.log(Thing);
         
         return (
             <div>
-                <YearElement year={title} />
+                <YearElement year={title} text={Year1Text}/>
                 <BranchingLine />
             </div>
         )
