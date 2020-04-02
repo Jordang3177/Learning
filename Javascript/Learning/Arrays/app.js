5133e08d12257c4241beef39fee5bac91069cc4e
Jordan Graves <Jordang3177@gmail.com>
2020-04-02 21:00:41 +0800

    Finished Section 8 part of Arrays

--- Javascript/Learning/Arrays/app.js
+++ Javascript/Learning/Arrays/app.js
@@ -28,7 +28,7 @@
 // hobbies.push('Reading');
 // hobbies.unshift('Coding');
 // hobbies.pop();
-// hobbies.shift(); 
+// hobbies.shift();
 // console.log(hobbies);
 
 // hobbies[1] = 'Coding';
@@ -40,7 +40,6 @@
 
 // hobbies.splice(0, 1); // splice also returns the elements that it removed
 // console.log(hobbies);
-
 
 // hobbies.splice(-1, 1);
 // console.log(hobbies);
@@ -60,7 +59,7 @@
 // console.log(personData.indexOf({name: 'Manuel'})); // because of the way objects work this will say there isn't anything in the array that is this same object.
 
 // const manuel = personData.find((person, idx, persons) => {
-//     return person.name === 'Manuel'; 
+//     return person.name === 'Manuel';
 // });
 
 // manuel.name = 'Anna'; // this will change the value in the array as well.
@@ -88,14 +87,57 @@
 
 // console.log(taxAdjustedPrices);
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
-const taxAdjustedPrices = prices.map((price, idx, prices) => {
-    const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
-    return priceObj;
-});
+// // const taxAdjustedPrices = prices.map((price, idx, prices) => {
+// //     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)};
+// //     return priceObj;
+// // });
 
-console.log(prices, taxAdjustedPrices)
+// // console.log(prices, taxAdjustedPrices)
 
+// const sum = prices.reduce(
+//   (prevValue, curValue) => prevValue + curValue,
+//   0
+// );
 
+// console.log(sum);
+
+// const data = "new york;10.99;2000";
+
+// const transformedData = data.split(";");
+// transformedData[1] = +transformedData[1];
+// console.log(transformedData);
+
+// const nameFragments = ["Max", "Schwarz"];
+// const name = nameFragments.join(" ");
+// console.log(name);
+
+// const copiedNameFragments = [...nameFragments];
+// nameFragments.push("Mr");
+// console.log(nameFragments, copiedNameFragments);
+
+// console.log(Math.min(1, 5, -3));
+// console.log(Math.min(...prices)); // console.log(Math.min(prices));
+
+// const persons = [
+//   { name: "Max", age: 30 },
+//   { name: "Manuel", age: 31 }
+// ];
+// const copiedPersons = [...persons];
+// const copiedPersons2 = persons.map(person => ({
+//   name: person.name,
+//   age: person.age
+// })); // This is a way to stop the idea of copying the reference but instead copying the values only and the data only.
+// persons.push({ name: "Anna", age: 29 });
+// persons[0].age = 31; //This will affect copiedPersons as well because the spread operator copies the address of each element. So any edits to the objects will also affect the new array.
+
+// console.log(persons, copiedPersons);
+
+// const nameData = ["Jordan", "Graves", "Ruolan", 24];
+// // const firstName = nameData[0];
+// // const lastName = nameData[1];
+
+// const [firstName, lastName, ...otherInformation] = nameData;
+// console.log(firstName, lastName, otherInformation); //otherInformation will be displayed as an array.
