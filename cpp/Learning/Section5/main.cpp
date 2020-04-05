4b8a08b89348ce399745d9c7f9bc0bbe3574f6a5
Jordan Graves <Jordang3177@gmail.com>
2020-04-05 19:56:41 +0800

    Section 5 Completed

---
+++ cpp/Learning/Section5/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World";
+
+    // cout << "Hello";
+    // cout << "World" << endl;
+
+    // cout << "Hello World" << endl;
+    // cout << "Hello"
+    //      << " World" << endl;
+    // cout << "Hello"
+    //      << "world\n";
+    // cout << "Hello"
+    //      << "world\n";
+    // cout << "Hello\nOut\nThere\n";
+
+    int num1;
+    int num2;
+    double num3;
+
+    // cout << "Enter an intger: ";
+    // cin >> num1;
+    // cout << "You entered: " << num1 << endl;
+
+    // cout << "Enter a first intger: ";
+    // cin >> num1;
+
+    // cout << "Enter a second Integer: ";
+    // cin >> num2;
+
+    // cout << "You entered " << num1 << " and " << num2 << endl;
+
+    // cout << "Enter 2 integers seperated with a space: ";
+    // cin >> num1 >> num2;
+    // cout << "You entered " << num1 << " and " << num2;
+
+    // cout << "Enter a double: ";
+    // cin >> num3;
+
+    // cout << "You entered " << num3 << endl;
+
+    cout << "Enter an integer ";
+    cin >> num1;
+
+    cout << "Enter a double: ";
+    cin >> num3;
+
+    cout << "The integer is: " << num1 << endl;
+    cout << "The double is: " << num3 << endl;
+}
